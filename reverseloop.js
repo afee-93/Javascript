f69ddcd7d598bf8124984bb4c073ae5a9da5cc5d
Abdul Afees M <149700481+afee-93@users.noreply.github.com>
2025-07-16 19:08:12 +0530

    Add files via upload

---
+++ reverseloop.js
@@ -0,0 +1,18 @@
+// For loop
+for (let m = 10; m >= 1; m--) {
+    console.log(m);
+}
+
+// While loop
+let i = 10;
+while (i >= 1) {
+    console.log(i);
+    i--;
+}
+
+// Do-while loop
+let j = 11;
+do {
+    j--;
+    console.log(j);
+} while (j >= 1);
